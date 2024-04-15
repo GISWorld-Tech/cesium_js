7010220b2d5b879a23a31d60d2e053a1f703245a
Mohammad <m.hgzadeh@gmail.com>
2024-04-15 22:35:00 +0200

    init hello world npm

---
+++ part_3/part_3_1/src/index.js
@@ -0,0 +1,2 @@
+console.log("Hello World!");
+import "../src/css/main.css";
---
+++ part_3/part_3_1/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ["url-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  mode: "development",
+};
