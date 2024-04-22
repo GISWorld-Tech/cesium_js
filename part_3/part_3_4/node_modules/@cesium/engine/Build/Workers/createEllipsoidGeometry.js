4a4e38501385911997dcd87772139f49f55ad157
Mohammad <m.hgzadeh@gmail.com>
2024-04-22 20:26:48 +0200

    test part3

---
+++ part_3/part_3_4/node_modules/@cesium/engine/Build/Workers/createEllipsoidGeometry.js
@@ -0,0 +1,59 @@
+/**
+ * @license
+ * Cesium - https://github.com/CesiumGS/cesium
+ * Version 1.116
+ *
+ * Copyright 2011-2022 Cesium Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Columbus View (Pat. Pend.)
+ *
+ * Portions licensed separately.
+ * See https://github.com/CesiumGS/cesium/blob/main/LICENSE.md for full licensing details.
+ */
+
+import {
+  EllipsoidGeometry_default
+} from "./chunk-RV7ZOSS6.js";
+import "./chunk-GXUXEOE2.js";
+import "./chunk-YV4H2Y7M.js";
+import "./chunk-P4XKB3QZ.js";
+import "./chunk-EOXYZ34O.js";
+import "./chunk-2ZBL3MVO.js";
+import "./chunk-7COR472O.js";
+import "./chunk-2KACWKS4.js";
+import "./chunk-X6XOMPWL.js";
+import "./chunk-2XPLH3JY.js";
+import "./chunk-NZWNZA6S.js";
+import "./chunk-ADOVHYWO.js";
+import "./chunk-QCQUZGXI.js";
+import "./chunk-I37B3MZ2.js";
+import "./chunk-N4KXMWQU.js";
+import "./chunk-TZORKRNK.js";
+import "./chunk-SS4ESNE3.js";
+import {
+  defined_default
+} from "./chunk-YDVSBWEO.js";
+
+// packages/engine/Source/Workers/createEllipsoidGeometry.js
+function createEllipsoidGeometry(ellipsoidGeometry, offset) {
+  if (defined_default(offset)) {
+    ellipsoidGeometry = EllipsoidGeometry_default.unpack(ellipsoidGeometry, offset);
+  }
+  return EllipsoidGeometry_default.createGeometry(ellipsoidGeometry);
+}
+var createEllipsoidGeometry_default = createEllipsoidGeometry;
+export {
+  createEllipsoidGeometry_default as default
+};
---
+++ part_3/part_3_4/src/index.js
@@ -0,0 +1,33 @@
+import {
+  Ion,
+  Viewer,
+  Terrain,
+  createOsmBuildingsAsync,
+  Cartesian3,
+  Math,
+} from "cesium";
+import "cesium/Widgets/widgets.css";
+import "../src/css/main.css";
+
+// Your access token can be found at: https://cesium.com/ion/tokens.
+// This is the default access token
+Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk";
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Viewer('cesiumContainer', {
+  terrain: Terrain.fromWorldTerrain(),
+});    
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: Math.toRadians(0.0),
+    pitch: Math.toRadians(-15.0),
+  },
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = await createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileset); 
