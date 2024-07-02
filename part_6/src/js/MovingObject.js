23a64be56b318d9ca6ddcaf87a67b7f9997f0f7c
Mohammad <m.hgzadeh@gmail.com>
2024-07-03 00:13:59 +0200

    add clamp to code

--- part_6/src/js/MovingObject.js
+++ part_6/src/js/MovingObject.js
@@ -1,19 +1,20 @@
 export class MovingObject {
-  constructor(viewer, filghtData, timeStepInSeconds) {
+  constructor(viewer, flightData, timeStepInSeconds) {
     this.viewer = viewer;
-    this.filghtData = filghtData;
+    this.flightData = flightData;
     this.timeStepInSeconds = timeStepInSeconds;
     this.start = Cesium.JulianDate.fromIso8601("2024-06-10T23:10:00Z");
     this.stop = Cesium.JulianDate.addSeconds(
       this.start,
-      this.filghtData.features.length * this.timeStepInSeconds,
+      this.flightData.features.length * this.timeStepInSeconds,
       new Cesium.JulianDate()
     );
-    this._confingTime();
-    this.positionProperty = this._computeTimePositionAdjastment();
+    this._configTime();
+    this.positionProperty = new Cesium.SampledPositionProperty();
+    this._computeTimePositionAdjustment();
   }
 
-  _confingTime = () => {
+  _configTime = () => {
     this.viewer.clock.startTime = this.start.clone();
     this.viewer.clock.stopTime = this.stop.clone();
     this.viewer.clock.currentTime = this.start.clone();
@@ -22,46 +23,55 @@ export class MovingObject {
     this.viewer.clock.shouldAnimate = true;
   };
 
-  _computeTimePositionAdjastment = () => {
-    const positionProperty = new Cesium.SampledPositionProperty();
-    this.filghtData.features.forEach((feature, i) => {
-      // Declare the time for this individual sample and store it in a new JulianDate instance.
-      const time = Cesium.JulianDate.addSeconds(
-        this.start,
-        i * this.timeStepInSeconds,
-        new Cesium.JulianDate()
-      );
-      let lon = feature.geometry.coordinates[0];
-      let lat = feature.geometry.coordinates[1];
-      let alt = feature.properties.height + feature.geometry.coordinates[2];
-      const position = Cesium.Cartesian3.fromDegrees(lon, lat, alt);
-      // Store the position along with its timestamp.
-      // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-      positionProperty.addSample(time, position);
-
-      this.viewer.entities.add({
-        description: `Location: (${lon}, ${lat}, ${alt})`,
-        position: position,
-        point: { pixelSize: 5, color: Cesium.Color.RED },
-      });
+  _computeTimePositionAdjustment = async () => {
+    const positions = this.flightData.features.map((feature) => {
+      const lon = feature.geometry.coordinates[0];
+      const lat = feature.geometry.coordinates[1];
+      return Cesium.Cartographic.fromDegrees(lon, lat);
     });
-    return positionProperty;
+
+    try {
+      // Sample the terrain to get the heights
+      const updatedPositions = await Cesium.sampleTerrainMostDetailed(
+        this.viewer.terrainProvider,
+        positions
+      );
+
+      updatedPositions.forEach((cartographic, i) => {
+        const feature = this.flightData.features[i];
+        const time = Cesium.JulianDate.addSeconds(
+          this.start,
+          i * this.timeStepInSeconds,
+          new Cesium.JulianDate()
+        );
+        const position = Cesium.Cartesian3.fromRadians(
+          cartographic.longitude,
+          cartographic.latitude,
+          cartographic.height
+        );
+        this.positionProperty.addSample(time, position);
+
+        this.viewer.entities.add({
+          description: `Location: (${cartographic.longitude}, ${cartographic.latitude}, ${cartographic.height})`,
+          position: position,
+          point: { pixelSize: 5, color: Cesium.Color.RED },
+        });
+      });
+    } catch (error) {
+      console.error("Error sampling terrain:", error);
+    }
   };
+
   addMovableEntityToViewer = (uri) => {
-    // Load the glTF model from Cesium ion.
     const airplaneEntity = this.viewer.entities.add({
       availability: new Cesium.TimeIntervalCollection([
         new Cesium.TimeInterval({ start: this.start, stop: this.stop }),
       ]),
       position: this.positionProperty,
-      // Attach the 3D model instead of the green point.
       model: { uri: uri },
-      // Automatically compute the orientation from the position.
-      orientation: new Cesium.VelocityOrientationProperty(
-        this.positionProperty
-      ),
+      orientation: new Cesium.VelocityOrientationProperty(this.positionProperty),
       path: new Cesium.PathGraphics({ width: 3 }),
-      viewFrom: new Cesium.Cartesian3(-100, 0, 100)
+      viewFrom: new Cesium.Cartesian3(-100, 0, 100),
     });
 
     this.viewer.trackedEntity = airplaneEntity;
