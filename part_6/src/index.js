5eb8adbd871204b4c25c9bdd0ed138b90ae18fb3
Mohammad <m.hgzadeh@gmail.com>
2024-07-11 17:18:17 +0200

    update codes

--- part_6/src/index.js
+++ part_6/src/index.js
@@ -8,7 +8,27 @@ import { MovingObject } from "./js/MovingObject.js";
 Cesium.Ion.defaultAccessToken = accessToken;
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Cesium.Viewer("cesiumContainer");
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = await Cesium.createOsmBuildingsAsync();
+viewer.scene.primitives.add(buildingTileset);
 
 const movingObject = new MovingObject(viewer, flightData, 1);
-movingObject.addMovableEntityToViewer(uriPath["aircraft"]);
+movingObject.addMovableEntityToViewer(uriPath["automobile"]);
+
+const addMovableEntity = (type, height, speed) => {
+  viewer.entities.removeAll();
+  const movingObject = new MovingObject(viewer, flightData, 1, height, speed);
+  movingObject.addMovableEntityToViewer(uriPath[type]);
+};
+
+document
+  .getElementById("btnautomobile")
+  .addEventListener("click", () => addMovableEntity("automobile"));
+
+document
+  .getElementById("btnaircraft")
+  .addEventListener("click", () => addMovableEntity("aircraft", 50, 10));
