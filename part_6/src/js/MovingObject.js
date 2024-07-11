9d75a01c7e8ab3b60320ecf4c76c16579c6bdb16
Mohammad <m.hgzadeh@gmail.com>
2024-07-11 16:16:08 +0200

    init part 6

--- part_6/src/js/MovingObject.js
+++ part_6/src/js/MovingObject.js
@@ -1,8 +1,10 @@
 export class MovingObject {
-  constructor(viewer, filghtData, timeStepInSeconds) {
+  constructor(viewer, filghtData, timeStepInSeconds, height = 0, speed = 10) {
     this.viewer = viewer;
     this.filghtData = filghtData;
     this.timeStepInSeconds = timeStepInSeconds;
+    this.height = height;
+    this.speed = speed;
     this.start = Cesium.JulianDate.fromIso8601("2024-06-10T23:10:00Z");
     this.stop = Cesium.JulianDate.addSeconds(
       this.start,
@@ -10,7 +12,8 @@ export class MovingObject {
       new Cesium.JulianDate()
     );
     this._confingTime();
-    this.positionProperty = this._computeTimePositionAdjastment();
+    this.positionProperty = new Cesium.SampledPositionProperty();
+    this._computeTimePositionAdjastment();
   }
 
   _confingTime = () => {
@@ -22,31 +25,42 @@ export class MovingObject {
     this.viewer.clock.shouldAnimate = true;
   };
 
-  _computeTimePositionAdjastment = () => {
-    const positionProperty = new Cesium.SampledPositionProperty();
-    this.filghtData.features.forEach((feature, i) => {
+  _computeTimePositionAdjastment = async () => {
+    const positions = this.filghtData.features.map((feature) => {
+      const lon = feature.geometry.coordinates[0];
+      const lat = feature.geometry.coordinates[1];
+      return Cesium.Cartographic.fromDegrees(lon, lat);
+    });
+
+    const updatedPostions = await Cesium.sampleTerrainMostDetailed(
+      this.viewer.terrainProvider,
+      positions
+    );
+
+    updatedPostions.forEach((cartographic, i) => {
       // Declare the time for this individual sample and store it in a new JulianDate instance.
       const time = Cesium.JulianDate.addSeconds(
         this.start,
         i * this.timeStepInSeconds,
         new Cesium.JulianDate()
       );
-      let lon = feature.geometry.coordinates[0];
-      let lat = feature.geometry.coordinates[1];
-      let alt = feature.properties.height;
-      const position = Cesium.Cartesian3.fromDegrees(lon, lat, alt);
+      const position = Cesium.Cartesian3.fromRadians(
+        cartographic.longitude,
+        cartographic.latitude,
+        cartographic.height
+      );
       // Store the position along with its timestamp.
       // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-      positionProperty.addSample(time, position);
+      this.positionProperty.addSample(time, position);
 
       this.viewer.entities.add({
-        description: `Location: (${lon}, ${lat}, ${alt})`,
+        description: `Location: (${cartographic.longitude}, ${cartographic.latitude}, ${cartographic.height})`,
         position: position,
         point: { pixelSize: 5, color: Cesium.Color.RED },
       });
     });
-    return positionProperty;
   };
+
   addMovableEntityToViewer = (uri) => {
     // Load the glTF model from Cesium ion.
     const airplaneEntity = this.viewer.entities.add({
@@ -60,8 +74,8 @@ export class MovingObject {
       orientation: new Cesium.VelocityOrientationProperty(
         this.positionProperty
       ),
-    //   path: new Cesium.PathGraphics({ width: 3 }),
-      viewFrom: new Cesium.Cartesian3(-100, 0, 100)
+      //   path: new Cesium.PathGraphics({ width: 3 }),
+      viewFrom: new Cesium.Cartesian3(-100, 0, 100),
     });
 
     this.viewer.trackedEntity = airplaneEntity;
