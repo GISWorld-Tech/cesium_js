ad72f40685c959cca1536f3c2c694b1d32f6a732
Mohammad <m.hgzadeh@gmail.com>
2024-08-18 10:06:26 +0200

    remove node_modules and dist

--- part_7/webpack/node_modules/chroma-js/src/utils/last.js
+++
@@ -1,8 +0,0 @@
-import type from './type.js';
-
-export default (args) => {
-    if (args.length < 2) return null;
-    const l = args.length - 1;
-    if (type(args[l]) == 'string') return args[l].toLowerCase();
-    return null;
-};
--- part_7/webpack/node_modules/chroma-js/src/utils/limit.js
+++
@@ -1,5 +0,0 @@
-import { min, max } from './index.js';
-
-export default (x, low = 0, high = 1) => {
-    return min(max(low, x), high);
-};
