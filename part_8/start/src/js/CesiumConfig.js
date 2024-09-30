97eb9aad381ff2ace1281a437c7e8784356e20a6
Mohammad <m.hgzadeh@gmail.com>
2024-09-30 16:49:02 +0200

    remove point cloud asset id

--- part_8/start/src/js/CesiumConfig.js
+++ part_8/start/src/js/CesiumConfig.js
@@ -1,6 +1,6 @@
 const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZDNjMzA1Mi04ZGNmLTQ5YTgtOThjZi00NzZhNWMzNj' +
   'FkZDIiLCJpZCI6MTkxNTA0LCJpYXQiOjE3MjM0OTE3MTh9.iv_r8IxVBItODjZWmSO8WjFCxSSrmu6rQ7qnGYxZNkM';
 
-const assetIds = { cityGml: 2729681, pointCloud: 2729281 };
+const assetId = 2729681;
 
-export { accessToken, assetIds };
+export { accessToken, assetId };
