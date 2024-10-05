743a4ead40de098505e38361168ffe45477aaa86
Mohammad <m.hgzadeh@gmail.com>
2024-10-05 23:21:13 +0200

    refactor code with axios

--- part_9/start/src/index.js
+++ part_9/start/src/index.js
@@ -2,11 +2,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import "./css/main.css";
 import "cesium/Build/Cesium/Widgets/widgets.css";
+import axios from "axios";
 
-// Define the API URL
 const apiUrl = "http://0.0.0.0:8000/cesium/polygon/?format=json";
 
-// Fetch the data
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => console.log("Polygon data:", data))
+const response = axios
+  .get(apiUrl)
+  .then(response => console.log(response.data))
+  .catch(error => console.log(error));
