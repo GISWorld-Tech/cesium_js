0d2077e84606006ad5044b5851514f1bda12b418
Mohammad <m.hgzadeh@gmail.com>
2024-10-07 12:13:37 +0200

    refactor code base on axios.all

--- part_9/start/src/index.js
+++ part_9/start/src/index.js
@@ -9,27 +9,38 @@ import { accessToken } from "./js/CesiumConfig";
 Ion.defaultAccessToken = accessToken;
 
 const viewer = new Viewer("cesiumContainer");
-const apiUrl = "http://0.0.0.0:8000/cesium/polygon/?format=json";
+const apiPolygonUrl = "https://gisworld-tech.com/cesium/polygon/?format=json";
+const apiPointUrl = "https://gisworld-tech.com/cesium/point/?format=json";
+const apiLinestringUrl = "https://gisworld-tech.com/cesium/linestring/?format=json";
 
-function fetchData(update = false) {
-  axios
-    .get(apiUrl)
-    .then(response => {
+const addCorridors = (features) => {
+  features.forEach(feature => {
+    viewer.entities.add({
+      name: feature.properties.usage,
+      corridor: {
+        positions: Cartesian3.fromDegreesArray(feature.geometry.coordinates[0].flat()),
+        width: 1,
+        material: Color.RED
+      }
+    });
+  });
+};
+
+const fetchAllData = (update = false) => {
+  axios.all([
+    axios.get(apiPolygonUrl),
+    axios.get(apiPointUrl),
+    axios.get(apiLinestringUrl)
+  ])
+    .then(axios.spread((response1, response2, response3) => {
       update && viewer.entities.removeAll();
-      response.data.features.forEach(feature => {
-        viewer.entities.add({
-          name: feature.properties.usage,
-          corridor: {
-            positions: Cartesian3.fromDegreesArray(feature.geometry.coordinates[0].flat()),
-            width: 1,
-            material: Color.RED.withAlpha(0.5)
-          }
-        });
-      });
-      !update && viewer.zoomTo(viewer.entities);
-    })
-    .catch(error => console.log(error));
-}
-fetchData();
+      addCorridors(response1.data.features);
 
-document.getElementById("fetchDataButton").addEventListener("click", () => fetchData(true));
+      !update && viewer.zoomTo(viewer.entities);
+    }))
+    .catch(error => console.log(error));
+};
+
+fetchAllData();
+
+document.getElementById("fetchDataButton").addEventListener("click", () => fetchAllData(true));
