271e1de8b12091e3d48063023a1d2ef1a74f17fc
Mohammad <m.hgzadeh@gmail.com>
2024-11-14 08:53:58 +0100

    add convert line to point

--- part_10/start/src/js/ConvertLineToPoint.js
+++ part_10/start/src/js/ConvertLineToPoint.js
@@ -1,15 +1,25 @@
 export const convertLineToPoints = (lineFeature) => {
   const pointFeatures = {
     type: "FeatureCollection",
-    name: "points",
+    name: "bus_points",
     crs: {
       type: "name",
       properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
     },
     features: [],
   };
-
-  // TODO 5.1: Convert line to point
+  const coordinates = lineFeature.geometry.coordinates;
+  coordinates.forEach((coord, index) => {
+    const pointFeature = {
+      type: "Feature",
+      properties: { fid: index + 1 },
+      geometry: {
+        type: "Point",
+        coordinates: coord,
+      },
+    };
+    pointFeatures.features.push(pointFeature);
+  });
 
   return pointFeatures;
 };
